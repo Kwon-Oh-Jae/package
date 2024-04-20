6bc675f21efebe9aa95f57219c50708a052e7119
Kwon-Oh-Jae <162390459+Kwon-Oh-Jae@users.noreply.github.com>
2024-04-21 04:58:06 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+    name = 'kojcalc',
+    version = '0.0.1',
+    description = 'nice calculator',
+    author = 'ojkwon',
+    url = 'https://github.com/Kwon-Oh-Jae/package.git',
+    download_url = 'https://github.com/Kwon-Oh-Jae/package.git',
+    packages = ['mycalc'],
+    classifiers= [
+      "Programming Language :: Python :: 3"
+    ]
+ )
